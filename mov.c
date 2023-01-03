2d04dc615b02cd2df18f8d32f32ae75158d38b01
rapgonca <rapgonca@student.42sp.org.br>
2023-01-03 15:20:44 +0100

    add map_validation/creat movimentation

---
+++ mov.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mov.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By:  rapgonca < rapgonca@student.42sp.org.b    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 14:43:13 by  rapgonca         #+#    #+#             */
+/*   Updated: 2023/01/02 15:49:19 by  rapgonca        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	intput_axis_x(int key, t_mlx *lib)
+{
+	set_image_mov(key, lib);
+	if (key == 'a')
+	{
+		if (lib->tab.tab[lib->player.y / 50] \
+		[(lib->player.x / 50) - 1] != '1')
+		{
+			lib->tab.steps += 1;
+			return (50);
+		}
+	}
+	else if (key == 'd')
+	{
+		if (lib->tab.tab [lib->player.y / 50] \
+		[(lib->player.x / 50) + 1] != '1')
+		{
+			lib->tab.steps += 1;
+			return (-50);
+		}
+	}
+	return (0);
+}
+
+int	intput_axis_y(int key, t_mlx *lib)
+{
+	set_image_mov(key, lib);
+	if (key == 'w')
+	{
+		if (lib->tab.tab[(lib->player.y / 50) - 1][lib->player.x / 50] != '1')
+		{
+			lib->tab.steps += 1;
+			return (50);
+		}
+	}
+	else
+	{
+		if (lib->tab.tab[(lib->player.y / 50) + 1][lib->player.x / 50] != '1')
+		{
+			lib->tab.steps += 1;
+			return (-50);
+		}
+	}
+	return (0);
+}
+
+int	recreat_map(t_mlx *lib)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < lib->tab.height)
+	{
+		x = 0;
+		while (x < lib->tab.width)
+		{
+			ft_printf("\rsteps: %d\r", lib->tab.steps);
+			recreat_img(x, y, lib->tab.tab[y][x], lib);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(lib->mlx, lib->win, lib->player.img, \
+	lib->player.x, lib->player.y);
+	return (0);
+}
+
+int	close_win(t_mlx *lib)
+{
+	mlx_clear_window(lib->mlx, lib->win);
+	mlx_destroy_window(lib->mlx, lib->win);
+	free(lib->tab.tab);
+	free(lib->mlx);
+	mlx_loop_end(lib->mlx);
+	ft_printf("steps: %d\n", lib->tab.steps);
+	exit(0);
+}
+
+int	inputs_key(int key, t_mlx *lib)
+{
+	if (key == 'a' || key == 'd')
+		lib->player.x -= intput_axis_x(key, lib);
+	else if (key == 's' || key == 'w')
+		lib->player.y -= intput_axis_y(key, lib);
+	else if (key == 65307)
+		close_win(lib);
+	colection(lib);
+	mlx_clear_window(lib->mlx, lib->win);
+	recreat_map(lib);
+	return (0);
+}
